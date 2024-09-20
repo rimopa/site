d422f5e3c200317c2529dbdc05c73bb9f4ec2596
RIMOPA <santino.rimondotto@gmail.com>
2024-09-20 11:10:24 -0300

    initial 2

--- tools/js/tools.js
+++ tools/js/tools.js
@@ -6,19 +6,17 @@ function toolsNav() {
         const parser = new DOMParser();
         const doc = parser.parseFromString(data, "text/html");
         const toolsNav = doc.querySelector("#toolsNav");
-        document.getElementById("toolsNav").innerHTML = toolsNav.outerHTML;
+        const newToolsNav = document.getElementById("toolsNav");
+        newToolsNav.innerHTML = toolsNav.innerHTML;
+        var lis = document.querySelectorAll("#toolsNav ul li");
+        lis.forEach((li) => {
+          var a = li.querySelector("a");
+          if (a.getAttribute("href") == window.location.pathname) {
+            li.setAttribute("hidden", 1);
+          }
+        });
       })
       .catch((error) => console.error("Error fetching toolsNav:", error));
-  });
-  var ases = document.querySelectorAll("#toolsNav a");
-  console.log(ases);
-  console.log(ases.length);
-  ases.forEach((ele) => {
-    ele.setAttribute("hidden", 1);
-
-    if (ele.href == window.location.pathname) {
-      ele.hidden = 1;
-    }
   });
 }
 toolsNav();
