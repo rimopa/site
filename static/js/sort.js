8e3ee39009e0bd61e3471bb4556c910d4c329e7a
RIMOPA <santino.rimondotto@gmail.com>
2025-05-13 18:41:27 -0300

    reorder... and stuff

--- static/js/sort.js
+++ static/js/sort.js
@@ -1,28 +1,57 @@
+const DOMelements = {
+  resultElement: document.getElementById("sortNames.result"),
+  warningElement: document.getElementById("sortNames.warning"),
+  outputSeparator: document.getElementById("sortNames.output"),
+  inputSeparator: document.getElementById("sortNames.input"),
+  textarea: document.getElementById("sortNames.names"),
+  searchElements: document.getElementById("sortNames.searchElements"),
+  caseSensitive: document.getElementById("sortNames.caseSensitive"),
+  deleteSpaces: document.getElementById("sortNames.deleteSpaces"),
+  nameOrder: document.getElementById("sortNames.nameOrder"),
+  reverse: document.getElementById("sortNames.reverse"),
+};
 function orderNames() {
-  function createRegex(pattern, caseSensitive, global = false) {
-    const flags = caseSensitive ? (global ? "g" : "") : global ? "gi" : "i";
-    return new RegExp(pattern, flags);
+  let sortedList = [];
+  //
+  let warnings = { nameErrorsI: [] };
+  const textElements = getNames();
+  if (textElements != false) {
+    let sortingList = [];
+    if (DOMelements.nameOrder.checked) {
+      const { sortingList: newSortingList, warnings: newWarnings } = processNameOrder(textElements);
+      sortingList = newSortingList;
+      warnings = { ...warnings, ...newWarnings };
+    } else {
+      for (let i = 0; i < textElements.length; i++) {
+        sortingList.push([textElements[i], i]);
+      }
+    }
+    if (!DOMelements.caseSensitive.checked) {
+      for (let i = 0; i < sortingList.length; i++) {
+        sortingList[i][0] = sortingList[i][0].toUpperCase();
+      }
+    }
+    sortedList = sortList(sortingList);
   }
+  printResult(sortedList);
+  printWarnings();
+
   function getNames() {
+    var elements;
     try {
       const regex = createRegex(
         DOMelements.inputSeparator.value,
         DOMelements.caseSensitive.checked,
         DOMelements.searchElements.checked
       );
-      return DOMelements.searchElements.checked
+      elements = DOMelements.searchElements.checked
         ? [...DOMelements.textarea.value.matchAll(regex)].map((match) => match[0])
         : DOMelements.textarea.value.split(regex);
     } catch (error) {
       warnings.regexpError = DOMelements.inputSeparator.value;
       return false;
     }
-  }
-  function trim(str) {
-    return str.replace(/^\s\s*/, "").replace(/\s\s*$/, "");
-  }
-  function trimElements(lst) {
-    return lst.map(trim);
+    return DOMelements.deleteSpaces.checked ? trimElements(elements) : elements;
   }
   function printResult(sortedList) {
     const result = sortedList.join(DOMelements.outputSeparator.value).replace(/\\n/g, "\n");
@@ -43,78 +72,42 @@ function orderNames() {
       empty: "Error. Empty result",
     };
 
-    DOMelements.warningElement.hidden = Object.keys(warnings).length === 0;
-    DOMelements.warningElement.innerHTML = Object.keys(warnings)
+    const messages = Object.keys(warnings)
       .map((key) => warningMessages[key])
       .join("<br>");
+    DOMelements.warningElement.hidden = !messages;
+    DOMelements.warningElement.innerHTML = messages;
   }
   function sortList(lst) {
-    lst.sort((a, b) => (a[0] < b[0] ? -1 : a[0] > b[0] ? 1 : 0));
-    return DOMelements.reverse.checked
-      ? lst.map((ele) => initialList[ele[1]]).reverse()
-      : lst.map((ele) => initialList[ele[1]]);
+    lst.sort((a, b) => a[0].localeCompare(b[0]));
+    const sorted = lst.map((ele) => textElements[ele[1]]);
+    return DOMelements.reverse.checked ? sorted.reverse() : sorted;
   }
   function processNameOrder(textElements) {
     const sortingList = [];
     const warnings = { nameErrorsI: [] };
-    let i = 0;
-    textElements.forEach((ele) => {
-      const inputtedWords = ele.split(" ").filter(Boolean);
-      const [firstName, middleName, ...lastNameParts] = inputtedWords;
 
+    textElements.forEach((ele, i) => {
+      const inputtedWords = ele.split(" ").filter(Boolean);
       if (inputtedWords.length < 1 || inputtedWords.length > 4) {
         warnings.nameErrorsI.push(i);
         return;
       }
-      initialList.push(textElements[i]);
+
+      const [firstName, middleName, ...lastNameParts] = inputtedWords;
       const lastName = lastNameParts.join(" ");
       const formattedName = [lastName, firstName, middleName].filter(Boolean).join(" ");
       sortingList.push([formattedName, i]);
-      i++;
     });
 
     return { sortingList, warnings };
   }
-  let sortedList = [];
-  let initialList = [];
-  //
-  let warnings = { nameErrorsI: [] };
-  const textElements = getNames();
-  if (textElements != false) {
-    let sortingList = [];
-    if (DOMelements.deleteSpaces.checked) {
-      trimElements(textElements);
-    }
-    if (DOMelements.nameOrder.checked) {
-      const { sortingList: newSortingList, warnings: newWarnings } = processNameOrder(textElements);
-      sortingList = newSortingList;
-      warnings = { ...warnings, ...newWarnings };
-    } else {
-      initialList = textElements;
-      for (let i = 0; i < textElements.length; i++) {
-        sortingList.push([textElements[i], i]);
-      }
-    }
-    if (!DOMelements.caseSensitive.checked) {
-      for (let i = 0; i < sortingList.length; i++) {
-        sortingList[i][0] = sortingList[i][0].toUpperCase();
-      }
-    }
-    sortedList = sortList(sortingList);
-  }
-  printResult(sortedList);
-  printWarnings();
 }
-const DOMelements = {
-  resultElement: document.getElementById("sortNames.result"),
-  warningElement: document.getElementById("sortNames.warning"),
-  outputSeparator: document.getElementById("sortNames.output"),
-  inputSeparator: document.getElementById("sortNames.input"),
-  textarea: document.getElementById("sortNames.names"),
-  searchElements: document.getElementById("sortNames.searchElements"),
-  caseSensitive: document.getElementById("sortNames.caseSensitive"),
-  deleteSpaces: document.getElementById("sortNames.deleteSpaces"),
-  nameOrder: document.getElementById("sortNames.nameOrder"),
-  reverse: document.getElementById("sortNames.reverse"),
-};
+function createRegex(pattern, caseSensitive, global = false) {
+  const flags = caseSensitive ? (global ? "g" : "") : global ? "gi" : "i";
+  return new RegExp(pattern, flags);
+}
+function trimElements(lst) {
+  return lst.map((str) => str.replace(/^\s\s*/, "").replace(/\s\s*$/, ""));
+}
 orderNames();
